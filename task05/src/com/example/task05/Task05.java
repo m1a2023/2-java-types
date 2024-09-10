9c16d207d2b604853736670ea230819853950d40
m1a2023 <mag2017go@gmail.com>
2024-09-10 15:45:19 +0500

    task05 was done

--- task05/src/com/example/task05/Task05.java
+++ task05/src/com/example/task05/Task05.java
@@ -2,20 +2,24 @@ package com.example.task05;
 
 public class Task05 {
 
+    public static boolean isEven(int x) {
+        return x % 2 == 0;
+    }
+
     public static String solution(int x) {
 
-        // TODO напишите здесь свою корректную реализацию этого метода, вместо сеществующей
+        // Done
 
-        return "FALSE";
+        while (x != 0) {
+            if (!isEven(x)) return "FALSE";
+            x /= 10;
+        }
+
+        return "TRUE";
     }
 
     public static void main(String[] args) {
-        // Здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
-        // например вот так:
-        /*
-        String result = solution(1234);
-        System.out.println(result);
-        */
+
     }
 
 }
